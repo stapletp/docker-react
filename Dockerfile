9237aa692d70cbe1ac1a959705e5bd9f7ab86898
Patrick Stapleton <stapletp@me.com>
2022-03-31 15:10:54 -0600

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:16-alpine as stage
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM nginx
+COPY --from=stage /app/build /usr/share/nginx/html
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('displays test on screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const stringElement = screen.getByText('Welcome, Patrick.');
+  expect(stringElement).toBeInTheDocument();
+});
+
+test('displays test on screen', () => {
+  render(<App />);
+  const stringElement = screen.getByText('Welcome, Patrick.');
+  expect(stringElement).toBeInTheDocument();
 });
